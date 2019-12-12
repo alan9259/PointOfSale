6c94b9a4ab75d8659d10f71fe8dc52476caa08f3
Alan <32184821+alan9259@users.noreply.github.com>
2019-12-12 18:52:34 +1300

    Update Program.cs

--- Grocery/Program.cs
+++ Grocery/Program.cs
@@ -1,4 +1,4 @@
-﻿using PointOfSale.Services;
+using PointOfSale.Services;
 using Repository;
 using System;
 
@@ -8,14 +8,30 @@ namespace Grocery
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
+            Console.WriteLine("Press q to quit");
 
-            var pricingRepo = new PricingRepo();
-            var checkOutService = new CheckOutService(pricingRepo);
+            var quit = false;
 
-            var output = checkOutService.CheckOut(input);
+            while (!quit)
+            {
+                Console.WriteLine("Enter product codes eg, AABBCC");
 
-            Console.WriteLine(output);
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    quit = true;
+                }
+                else
+                {
+                    var pricingRepo = new PricingRepo();
+                    var checkOutService = new CheckOutService(pricingRepo);
+
+                    var output = checkOutService.CheckOut(input);
+
+                    Console.WriteLine(output);
+                }
+            }
         }
     }
 }
